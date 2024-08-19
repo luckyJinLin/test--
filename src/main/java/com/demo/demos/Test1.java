c7f717f1bd8176f4f7eb802d7b0374410d1367c3
jinli <15052515935@163.com>
2024-08-19 16:58:51 +0800

    dev新增回退测试3、4

--- src/main/java/com/demo/demos/Test1.java
+++ src/main/java/com/demo/demos/Test1.java
@@ -5,6 +5,8 @@ public class Test1 {
     public static void main(String[] args) {
         System.out.println("-----回退测试1-----");
         System.out.println("-----回退测试2-----");
+        System.out.println("-----dev新增回退测试3-----");
+        System.out.println("-----dev新增回退测试4-----");
     }
 
 }
