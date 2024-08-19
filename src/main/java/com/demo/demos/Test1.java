9e011f6bad025d207bc631380d777f597ad19a01
jinli <15052515935@163.com>
2024-08-19 16:29:14 +0800

    init 回退测试

---
+++ src/main/java/com/demo/demos/Test1.java
@@ -0,0 +1,10 @@
+package com.demo.demos;
+
+public class Test1 {
+
+    public static void main(String[] args) {
+        System.out.println("-----回退测试1-----");
+        System.out.println("-----回退测试2-----");
+    }
+
+}
